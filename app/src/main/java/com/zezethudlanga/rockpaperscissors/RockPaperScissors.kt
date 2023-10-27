74f3f6c1e3299ee163da53e9e64d1879ff2b96a6
Zezethu Dlanga <zezethudlanga@gmail.com>
2023-10-27 19:20:40 +0200

    Change if statement to when statement

--- app/src/main/java/com/zezethudlanga/rockpaperscissors/RockPaperScissors.kt
+++ app/src/main/java/com/zezethudlanga/rockpaperscissors/RockPaperScissors.kt
@@ -9,13 +9,25 @@ fun main() {
 
     val randomNumber = (1..3).random()
 
-    if (randomNumber == 1) {
-        computerChoice = "Rock"
-    } else if (randomNumber == 2) {
-        computerChoice = "Paper"
-    } else if (randomNumber == 3) {
-        computerChoice = "Scissors"
+    when (randomNumber) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissors"
+        }
     }
 
-    println(computerChoice)
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+
+    println(winner)
 }
